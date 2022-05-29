7f59d875efbc6cffb59b7e31db78c9d5d4cece9c
masatoshi.yamaguchi <masatoshi.yamaguchi@asia-quest.jp>
2022-05-29 16:58:03 +0900

    最初のテスト

---
+++ app/laravel/tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostListControllerTest extends TestCase
+{
+    /**  @test */
+    function ブログ一覧が表示される(){
+        $this->get('/')->assertOk();
+    }
+}
